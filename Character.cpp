df7a5f2de744e57186ffe9d771350bef838c4d1a
Heo Jin Woong <hjw001107@dgist.ac.kr>
2020-12-05 14:44:26 +0900

    character.cpp만듦

---
+++ Character.cpp
@@ -0,0 +1,70 @@
+#include "Character.h"
+#include "Location.h"
+#include "Enums.h"
+#include "Map.h"
+
+    void Character::MovePlayer(const int& direction, const Map& map) {
+        int height = map.GetMapHeight();
+        int width = map.GetMapWidth();
+        switch (direction) {
+            //캐릭터가 상,하,좌,우로 한 칸씩 움직인다. 목적지가 캐릭터가 이동할 수 없는 Block일 경우 움직이지 않는다.
+        case DIRECTION_UP:
+            if (y_pos > 0 && map.GetMapBlock(x_pos, y_pos - 1) != 1) {
+                y_pos--;
+            }
+            break;
+        case DIRECTION_DOWN:
+            if (y_pos < height - 1 && map.GetMapBlock(x_pos, y_pos + 1) != 1) {
+                y_pos++;
+            }
+            break;
+        case DIRECTION_RIGHT:
+            if (x_pos < width - 1 && map.GetMapBlock(x_pos + 1, y_pos) != 1) {
+                x_pos++;
+            }
+            break;
+        case DIRECTION_LEFT:
+            if (x_pos > 0 && map.GetMapBlock(x_pos - 1, y_pos) != 1) {
+                x_pos--;
+            }
+            break;
+
+            //위의 명령어를 for문을 통해 다섯 번 반복한다. 다섯 칸씩 움직일 수 있다.
+            //목적지가 캐릭터가 이동할 수 없는 장소일 경우 그 전까지만 이동한다.
+        case DIRECTION_FASTUP:
+            for (int i = 0; i < 5; i++) {
+                if (y_pos > 0 && map.GetMapBlock(x_pos, y_pos - 1) != 1) {
+                    y_pos--;
+                }
+            }
+            break;
+        case DIRECTION_FASTDOWN:
+            for (int i = 0; i < 5; i++) {
+                if (y_pos < height - 1 && map.GetMapBlock(x_pos, y_pos + 1) != 1) {
+                    y_pos++;
+                }
+            }
+            break;
+        case DIRECTION_FASTRIGHT:
+            for (int i = 0; i < 5; i++) {
+                if (x_pos < width - 1 && map.GetMapBlock(x_pos + 1, y_pos) != 1) {
+                    x_pos++;
+                }
+            }
+            break;
+        case DIRECTION_FASTLEFT:
+            for (int i = 0; i < 5; i++) {
+                if (x_pos > 0 && map.GetMapBlock(x_pos - 1, y_pos) != 1) {
+                    x_pos--;
+                }
+            }
+            break;
+        }
+    }
+
+void Character::PrintInformation() {
+    CursorPos(x_pos * 2, y_pos);
+    ChangeColor(COLOR_YELLOW, COLOR_DARKGRAY);
+    std::cout << "●";
+    ChangeColor();
+}
--- Character.h
+++ Character.h
@@ -20,70 +20,8 @@ public:
     //캐릭터를 지도상에서 움직이는 함수이다.
     //지도의 방향, 지도 객체를 매개변수로 이용한다.
     //지도를 넘어가지 않으면서 길, 건물, 조형물만 이동할 수 있도록 제한되어있다.
-    void MovePlayer(const int& direction, const Map& map) {
-        int height = map.GetMapHeight();
-        int width = map.GetMapWidth();
-        switch (direction) {
-            //캐릭터가 상,하,좌,우로 한 칸씩 움직인다. 목적지가 캐릭터가 이동할 수 없는 Block일 경우 움직이지 않는다.
-        case DIRECTION_UP:
-            if (y_pos > 0 && map.GetMapBlock(x_pos, y_pos - 1) != 1) {
-                y_pos--;
-            }
-            break;
-        case DIRECTION_DOWN:
-            if (y_pos < height - 1 && map.GetMapBlock(x_pos, y_pos + 1) != 1) {
-                y_pos++;
-            }
-            break;
-        case DIRECTION_RIGHT:
-            if (x_pos < width - 1 && map.GetMapBlock(x_pos + 1, y_pos) != 1) {
-                x_pos++;
-            }
-            break;
-        case DIRECTION_LEFT:
-            if (x_pos > 0 && map.GetMapBlock(x_pos - 1, y_pos) != 1) {
-                x_pos --;
-            }
-            break;
-
-            //위의 명령어를 for문을 통해 다섯 번 반복한다. 다섯 칸씩 움직일 수 있다.
-            //목적지가 캐릭터가 이동할 수 없는 장소일 경우 그 전까지만 이동한다.
-        case DIRECTION_FASTUP:
-            for (int i = 0; i < 5; i++) {
-                if (y_pos > 0 && map.GetMapBlock(x_pos, y_pos - 1) != 1) {
-                    y_pos--;
-                }
-            }
-            break;
-        case DIRECTION_FASTDOWN:
-            for (int i = 0; i < 5; i++) {
-                if (y_pos < height - 1 && map.GetMapBlock(x_pos, y_pos + 1) != 1) {
-                    y_pos++;
-                }
-            }
-            break;
-        case DIRECTION_FASTRIGHT:
-            for (int i = 0; i < 5; i++) {
-                if (x_pos < width - 1 && map.GetMapBlock(x_pos + 1, y_pos) != 1) {
-                    x_pos++;
-                }
-            }
-            break;
-        case DIRECTION_FASTLEFT:
-            for (int i = 0; i < 5; i++) {
-                if (x_pos > 0 && map.GetMapBlock(x_pos - 1, y_pos) != 1) {
-                    x_pos--;
-                }
-            }
-            break;
-        }
-    }
+    void MovePlayer(const int& direction, const Map& map);
 
     //캐릭터를 지도에 출력하는 함수이다.
-    void PrintInformation() override final {
-        CursorPos(x_pos * 2, y_pos);
-        ChangeColor(COLOR_YELLOW, COLOR_DARKGRAY);
-        std::cout << "●";
-        ChangeColor();
-    }
+    void PrintInformation() override final;
 };
